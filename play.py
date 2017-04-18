94395bf207635eaa4aa052add0c121e43b0569ad
Svante Kvarnström <sjk@ankeborg.nu>
2017-04-18 19:14:47 +0200

    Oh, progress

--- play.py
+++ play.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import subprocess
+import os
+import shlex
+import sys
+
+# subprocess32 is a backport of python3's subprocess module. It fixes
+# some bugs and adds some features.
+if os.name == 'posix' and sys.version_info[0] < 3:
+    import subprocess32 as subprocess
+else:
+    import subprocess
+
+LOGFILE = '~/Code/avpr/play.log'
 
 def play_loop(filename):
     """Loop video file
@@ -9,7 +20,29 @@ def play_loop(filename):
     :param filename: TODO
     :returns: TODO
 
-    OSError on no such file
+    :raises:
+        FileNotFoundError if filename does not exist
     """
 
+    filename = os.path.expanduser(filename)
 
+    if not os.path.isfile(filename):
+        raise IOError("Could not open {}".format(filename))
+
+    command = ("omxplayer "
+               "-o hdmi "
+               "--loop "
+               "--no-osd "
+               "--no-keys "
+               "{}".format(filename))
+
+    p = subprocess.Popen(shlex.split(command))
+
+    print("Pid: {}".format(p.pid))
+
+
+def main():
+    play_loop("instagramloop_avp_3.mp4")
+
+if __name__ == "__main__":
+    main()
