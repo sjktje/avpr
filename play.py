3f2c0ad4e0be4d1f57a1060be73fd04f7ffe6ef2
Svante Kvarnström <sjk@ankeborg.nu>
2017-05-09 12:23:18 +0200

    Init

---
+++ play.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+def play_loop(filename):
+    """Loop video file
+
+    :param filename: TODO
+    :returns: TODO
+
+    OSError on no such file
+    """
+
+
