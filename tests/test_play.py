d29453c34caf69b9a477e87522dd3a0e145f8c58
Svante Kvarnström <sjk@ankeborg.nu>
2017-05-10 15:06:14 +0200

    init test_play.py

---
+++ tests/test_play.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from avpr.play import main, parse_args
+
+def test_parse_args_kill():
+    args = parse_args(['--kill'])
+    assert args.kill is True
