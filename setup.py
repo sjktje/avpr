f3caedb60444fd99460ecf3d4f62e356c6a551bb
Svante Kvarnström <sjk@ankeborg.nu>
2017-05-11 15:51:41 +0200

    Make setuptools read version from avpr/__init__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 from setuptools import setup
+
+def get_version():
+    """Get version string
+
+    Got this from the pytest project's setup.py
+
+    :returns: version string in format x.y.z, for example 0.0.1
+    :license: MIT
+
+    """
+    path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'avpr',
+            '__init__.py'
+            )
+    with open(path, 'r') as file:
+        for line in file.readlines():
+            if '__version__' in line:
+                return line.strip().split('=')[-1].strip(" '")
+    raise ValueError('Could not read version from avpr/__init__.py')
+
 
 setup(
     name = "play",
@@ -9,7 +32,7 @@ setup(
     entry_points = {
         "console_scripts": ['play = avpr.play:main']
         },
-    version = '0.0.1',
+    version = get_version(),
     description = 'Play and loop a video file.',
     author = u'Svante Kvarnström',
     author_email = 'sjk@sjk.io',
